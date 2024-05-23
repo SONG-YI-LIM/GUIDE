bf25b67c854a69bb1d1f4a464c9d2919019f851c
SONG-YI-LIM <iamlsy.da@gmail.com>
2024-05-23 17:31:34 +0900

    2024-05-23

--- assets/js/common.js
+++ assets/js/common.js
@@ -72,79 +72,105 @@ const common = {
         });
     },
 }
+function fn_resize(){
+    let windowW = window.innerWidth;
+    windowW < 1161 ? $('#header').addClass('mob') : $('#header').removeClass('mob');
+}
 
 /* 웹 : 전체메뉴 */ 
 const fn_layout = () => {
+    $(window).resize($.throttle(250, fn_resize));
 
-    // resize evnet
-    // $(window).resize($throttle(100, function () {
-    //     console.log("hello");
-        
-    //     // let windowW = window.innerWidth;
-    //     // windowW < 1161 ? $('#header').addClass('mob') : $('#header').removeClass('mob')
-    // })().resize()
-
-    const GNB_MENU = $('.head-gnb .gnb .mn');
-    const GNB_MENU_ACT = $('.head-gnb .gnb .mn.active');
-    const GNB_SUBMENU = $('.head-gnb .gnb .sm');
-    const GNB_SUBMENU_ACT = $('.head-gnb .gnb .sm.active');
-
-    $('body').append("<div class='w-gnb-dim'></div>");
-    $('.w-gnb-dim').css({'display':'none'});
-
-    GNB_MENU.each(function(){
-        if(GNB_SUBMENU_ACT.length > 0){
-            GNB_SUBMENU_ACT.each(function(){
-                const $id = $(this).attr('data-id');
-                const $smAct = $('#'+$id);
-                $smAct.addClass('active');
-            });
-        }
-
-        $(this).on('click', function(){
-            if($(this).hasClass('open')){
-                GNB_MENU.removeClass('open');
-                GNB_MENU.next().removeClass('is-open');
-                $('.w-gnb-dim').css({'display':'none'});
-                
-            }else{
-                GNB_MENU.removeClass('open');
-                GNB_MENU.next().removeClass('is-open');
-                $('.w-gnb-dim').css({'display':'block'});
-                $(this).addClass('open');
-                $(this).next().addClass('is-open');
-            }
-
-            $(document).on('click', function(e){
-                if(e.target.closest('.w-gnb-dim')){
-                    GNB_MENU.next().removeClass('is-open');
-                    GNB_MENU.removeClass('active, open');
-                    $('.w-gnb-dim').css({'display':'none'});
-                    if(GNB_MENU.hasClass('active')){
-                        GNB_MENU.find('.sr-only').text('열기');
-                        GNB_MENU.removeClass('active');
-                    }
-                }
-            });
-        });
-    });
-
-    if(GNB_SUBMENU.length > 0){
-        GNB_SUBMENU.each(function(){
-            $(this).on('click', function(){
-                if(!$(this).hasClass('active')){
+    if(!$('#header').hasClass('mob')){
+        const GNB_MENU = $('.head-gnb .gnb .mn');
+        const GNB_MENU_ACT = $('.head-gnb .gnb .mn.active');
+        const GNB_SUBMENU = $('.head-gnb .gnb .sm');
+        const GNB_SUBMENU_ACT = $('.head-gnb .gnb .sm.active');
+    
+        $('body').append("<div class='w-gnb-dim'></div>");
+        $('.w-gnb-dim').css({'display':'none'});
+    
+        GNB_MENU.each(function(){
+            if(GNB_SUBMENU_ACT.length > 0){
+                GNB_SUBMENU_ACT.each(function(){
                     const $id = $(this).attr('data-id');
                     const $smAct = $('#'+$id);
-                    const $sm = $(this).parent().siblings().children();
-                    const $submenu = $(this).closest(".menu-wrap").siblings().children();
-                    $sm.removeClass('active');
-                    $submenu.removeClass('active');
-                    $(this).addClass('active');
                     $smAct.addClass('active');
+                });
+            }
+    
+            $(this).on('click', function(){
+                if($(this).hasClass('open')){
+                    GNB_MENU.removeClass('open');
+                    GNB_MENU.next().removeClass('is-open');
+                    $('.w-gnb-dim').css({'display':'none'});
+                    $('body').css({'overflow':'visible'});
+                }else{
+                    GNB_MENU.removeClass('open');
+                    GNB_MENU.next().removeClass('is-open');
+                    $('.w-gnb-dim').css({'display':'block'});
+                    $(this).addClass('open');
+                    $(this).next().addClass('is-open');
+                    $('body').css({'overflow':'hidden'});
                 }
+    
+                $(document).on('click', function(e){
+                    if(e.target.closest('.w-gnb-dim')){
+                        GNB_MENU.next().removeClass('is-open');
+                        GNB_MENU.removeClass('active, open');
+                        $('.w-gnb-dim').css({'display':'none'});
+                        if(GNB_MENU.hasClass('active')){
+                            GNB_MENU.find('.sr-only').text('열기');
+                            GNB_MENU.removeClass('active');
+                            $('body').css({'overflow':'visible'});
+                        }
+                    }
+                });
             });
         });
+    
+        if(GNB_SUBMENU.length > 0){
+            GNB_SUBMENU.each(function(){
+                $(this).on('click', function(){
+                    if(!$(this).hasClass('active')){
+                        const $id = $(this).attr('data-id');
+                        const $smAct = $('#'+$id);
+                        const $sm = $(this).parent().siblings().children();
+                        const $submenu = $(this).closest(".menu-wrap").siblings().children();
+                        $sm.removeClass('active');
+                        $submenu.removeClass('active');
+                        $(this).addClass('active');
+                        $smAct.addClass('active');
+                    }
+                });
+            });
+        }
+    
+        $(window).scroll(function(){
+            let $lastScrollY = 0;
+            const GWRAP = $('#g-wrap');
+            const HEADER = $('#header');
+            const $conOffsetTop = $('#container').offset().top;
+            const $scrollY = $(window).scrollTop();
+            const $scrollDown = $scrollY > $lastScrollY;
+            const $scrollUp = $scrollY < $lastScrollY;
+            if($scrollY > $conOffsetTop + 50 && $scrollDown){
+                GWRAP.removeClass('scroll-up');
+                GWRAP.addClass('scroll-down');
+            }else if($scrollY > $conOffsetTop + 50 && $scrollUp){
+                GWRAP.removeClass('scroll-down');
+                GWRAP.addClass('scroll-up');
+            }else{
+                GWRAP.removeClass('scroll-up, scroll-down');
+            }
+    
+    
+            console.log($scrollY, $conOffsetTop, $conOffsetTop+50)
+            ;
+        });
+
     }
+
 }
 
 
